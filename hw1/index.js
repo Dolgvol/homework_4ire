fe77b774dca464773860c588ac2d06c6e2c63f7a
Ivar <ivan@example.com>
2021-11-06 20:19:50 +0200

    hw1 fix2

--- hw1/index.js
+++ hw1/index.js
@@ -10,31 +10,41 @@ do {
    let firstCurrencyInput = null;
    let firstCurrency = null;
       do {
-         firstCurrencyInput = prompt('Выберите из списка название валюты которую хотите перевести и введите его в поле ниже: \n USD \n EUR \n GBP \n UAH \n RUB \n(в любом регистре)', '').trim().toUpperCase();
-         if (firstCurrencyInput === 'USD') {
-            firstCurrency = usd;
-         } else if (firstCurrencyInput === 'EUR') {
-            firstCurrency = eur;
-         } else if (firstCurrencyInput === 'GBP') {
-            firstCurrency = gbp;
-         } else if (firstCurrencyInput === 'UAH') {
-            firstCurrency = uah;
-         } else if (firstCurrencyInput === 'RUB') {
-            firstCurrency = rub;
-         } else {
-            alert('Неверное значение!');
-         }
+          let firstInput = prompt('Выберите из списка название валюты которую хотите перевести и введите его в поле ниже: \n USD \n EUR \n GBP \n UAH \n RUB \n(в любом регистре)', '');
+          if (firstInput !== null) {
+            firstCurrencyInput = firstInput.trim().toUpperCase();
+            if (firstCurrencyInput === 'USD') {
+               firstCurrency = usd;
+            } else if (firstCurrencyInput === 'EUR') {
+               firstCurrency = eur;
+            } else if (firstCurrencyInput === 'GBP') {
+               firstCurrency = gbp;
+            } else if (firstCurrencyInput === 'UAH') {
+               firstCurrency = uah;
+            } else if (firstCurrencyInput === 'RUB') {
+               firstCurrency = rub;
+            } else {
+               alert('Неверное значение!');
+            }
+          } else {
+            alert('Введите значение!');
+          }
       } while (!firstCurrency);
       
 
    let firstAmountInput = null;
    let firstAmount = null;
       do {
-         firstAmountInput = parseFloat(prompt(`Введите сумму ${firstCurrencyInput}, которую хотите перевести`, ''));   
-         if (isFinite(firstAmountInput)) {
-            firstAmount = parseFloat(firstAmountInput.toFixed(2));
+         let firstNumber = prompt(`Введите сумму ${firstCurrencyInput}, которую хотите перевести`, '');
+         if (firstNumber !== null) {
+            firstAmountInput = parseFloat(firstNumber);
+            if (isFinite(firstAmountInput)) {
+               firstAmount = parseFloat(firstAmountInput.toFixed(2));
+            } else {
+               alert('Неверное значение!');
+            }
          } else {
-            alert('Неверное значение!');
+            alert('Введите значение!');
          }
       } while (!firstAmount);
       
@@ -42,20 +52,25 @@ do {
    let secondCurrencyInput = null;
    let secondCurrency = null;
    do {
-      secondCurrencyInput = prompt('Выберите из списка название валюты которую хотите перевести и введите его в поле ниже: \n USD \n EUR \n GBP \n UAH \n RUB \n(в любом регистре)', '').trim().toUpperCase();   
-      if (secondCurrencyInput === 'USD') {
-         secondCurrency = usd;
-      } else if (secondCurrencyInput === 'EUR') {
-         secondCurrency = eur;
-      } else if (secondCurrencyInput === 'GBP') {
-         secondCurrency = gbp;
-      } else if (secondCurrencyInput === 'UAH') {
-         secondCurrency = uah;
-      } else if (secondCurrencyInput === 'RUB') {
-         secondCurrency = rub;
+      let secondInput = prompt('Выберите из списка название валюты которую хотите перевести и введите его в поле ниже: \n USD \n EUR \n GBP \n UAH \n RUB \n(в любом регистре)', ''); 
+      if (secondInput !== null) {
+         secondCurrencyInput = secondInput.trim().toUpperCase();
+         if (secondCurrencyInput === 'USD') {
+            secondCurrency = usd;
+         } else if (secondCurrencyInput === 'EUR') {
+            secondCurrency = eur;
+         } else if (secondCurrencyInput === 'GBP') {
+            secondCurrency = gbp;
+         } else if (secondCurrencyInput === 'UAH') {
+            secondCurrency = uah;
+         } else if (secondCurrencyInput === 'RUB') {
+            secondCurrency = rub;
+         } else {
+            alert('Неверное значение!');
+         }
       } else {
-         alert('Неверное значение!');
-      }
+         alert('Введите значение!');
+       }
    } while (!secondCurrency);
 
    let secondAmount = ((firstAmount * secondCurrency) / firstCurrency).toFixed(2);
