d7ad8b3a7dc13680b966ac848be85ca262d12fbc
Ivar <ivan@example.com>
2021-11-10 02:35:36 +0200

    hw3 added_second_variant final

--- hw3/index.js
+++ hw3/index.js
@@ -1,4 +1,6 @@
 
+inputArr = [1,null,2,'ads',undefined,3,'khjihi',[1,2,3],{},true,false,100n,Symbol("id"),filterBy];
+
 function filterBy(array, type) {
    if (type === 'null') {
       return array.filter(element => element !== null);
@@ -8,6 +10,38 @@ function filterBy(array, type) {
       return array.filter(element => typeof(element) !== type || typeof(element) === 'function');
    }
 }
-
-inputArr = [1,null,2,'ads',undefined,3,'khjihi',[1,2,3],{},true,false,100n,Symbol("id"),filterBy];
 console.log(filterBy(inputArr, 'object'));
+
+
+// более сложный вариант с заданным массивом названий типов и допобработкой входящего значения
+function filterBy2(array, type) {
+   if (typeof type === 'string') {
+      let pureType = type.toLowerCase().trim();
+      const allTypes = [
+         'null',
+         'undefined',
+         'string',
+         'number',
+         'bigint',
+         'boolean',
+         'symbol',
+         'object'   
+      ];
+      if (allTypes.includes(pureType)) {
+         if (pureType === 'null') {
+            return array.filter(element => element !== null);
+         } else if (pureType === 'object') {
+            return array.filter(element => typeof(element) !== pureType && typeof(element) !== 'function' || element === null);
+         } else {
+            return array.filter(element => typeof(element) !== pureType);
+         }
+      } else {
+         console.log('введно неверное значение типа!');
+         return array;
+      }
+   } else {
+      console.log('аргумент type принимает только строку, введите ее!');
+      return array;
+   }
+}
+// console.log(filterBy2(inputArr, undefined));
